cb811bd36397b4f3290774fa5f33bf76fdc2db45
ravshanmahmudov <ravshanmahmudov882@gmail.com>
2024-05-17 03:07:03 +1400

    js masala

---
+++ js/index.js
@@ -0,0 +1,105 @@
+// ===1chi masala===
+
+// let L = +prompt("uzunlikni sm larda kiriting");
+// let a = Math.floor(L/100);
+// console.log(a);
+
+// ===2chi masala===
+
+// let M = +prompt("kilogramda kiriting");
+// let a = Math.floor(1000/M);
+// console.log(a);
+
+// ===3chi masala===
+
+// let B = +prompt("FAYIL XAJIMI BAYITLARDA BULSIN");
+// let a = Math.floor(1024/B);
+// console.log(a);
+
+// ===4chi masala===
+
+// alert('A>B');
+// let B = +prompt("");
+// let A = +prompt("");
+// let a = Math.floor(A/B);
+// console.log(a);
+
+// ===5chi masala===
+
+// alert('A>B');
+// let B = +prompt("");
+// let A = +prompt("");
+// let N = Math.floor(A%B);
+// console.log(N);
+
+// ===6chi masala===
+
+// alert('2 XONALI SON KIRITING');
+// let B = +prompt("B");
+// let N = Math.floor(B/10);
+// console.log(N);
+// let C = Math.floor(B%10);
+// console.log(C);
+
+// ===7chi masala===
+
+// alert('2 XONALI SON KIRITING');
+// let B = +prompt("B");
+// let N = Math.floor(B/10);
+// console.log(N);
+// let C = Math.floor(B%10);
+// console.log(C);
+// let D = Math.floor(C+N);
+// console.log(D);
+
+// ===8chi masala===
+
+// alert('2 XONALI SON KIRITING');
+// let B = +prompt("B");
+// let N = Math.floor(B/10);
+// let C = Math.floor(B%10);
+// let D = Math.floor((C*10)+N);
+// console.log(D);
+// ===9chi masala===
+
+// alert('3 XONALI SON KIRITING');
+// let B = +prompt("B");
+// let N = Math.floor(B/100);
+// console.log(N);
+// ===10chi masala===
+// alert('3 XONALI SON KIRITING');
+// let B = +prompt("B");
+// let N = Math.floor(B%10);
+// let D = Math.floor(B/10%10);
+// console.log(N);
+// console.log(D);
+// ===11chi masala===
+
+// alert('99<1000');
+// let B = +prompt("B");
+// let N = Math.floor(B/100);
+// let C = Math.floor(B/10%10);
+// let D = Math.floor(B%10);
+// console.log(C+N+D);
+
+// ===12chi masala===
+
+// alert('3 XONALI SON KIRITING');
+// let B = +prompt("B");
+// let b = Math.floor(B%10);
+// let o = Math.floor(B/10%10);
+// let y = Math.floor(B/100);
+// console.log((b*100)+(o*10)+y);
+
+// ===13chi masala===
+
+// alert('3 XONALI SON KIRITING')
+// let B = +prompt("B");
+// let b = Math.floor(B/100);
+// let o = Math.floor(B/10%10);
+// let y = Math.floor(B%10);
+// console.log((o*100)+(y*10)+b);
+
+
+
+
